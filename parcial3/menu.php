a85cd0f13b3392e67dbdfa9d5ac500c279281b3b
josephEloirM <124744328+josephEloirM@users.noreply.github.com>
2023-11-23 00:06:15 -0700

    Add files via upload

--- parcial3/menu.php
+++ parcial3/menu.php
@@ -1,23 +1,28 @@
-<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #a7201f;">
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">CETIS 107</a>
+    <a class="navbar-brand" href="home.php">
+  <img src="logocetis2.png" alt="" width="40px" height="40px" class="d-inline-block aling-text-top">
+    CETIS 107
+  </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="inicio.php">Inicio</a>
+          <a class="nav-link active" aria-current="page" href="home.php">Inicio</a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Opciones
+            Alumnos
           </a>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="inicio.php">Inicio</a></li>
             <li><a class="dropdown-item" href="consultarAlumnos.php">Consultar alumnos</a></li>
             <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="RegistrarAlumno.php">Registar alumno</a></li>
+            <li><a class="dropdown-item" href="RegistrarAlumno.php">Registrar alumno</a></li>            
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="AsignarMaterias.php">Asignar materias</a></li>
           </ul>
         </li>
         <li class="nav-item dropdown">
@@ -25,16 +30,16 @@
             Materias
           </a>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="inicio.php">Inicio</a></li>
-            <li><a class="dropdown-item" href="consultarMaterias.php">Consultar Materias</a></li>
+            <li><a class="dropdown-item" href="consultarMaterias.php">Consultar materias</a></li>
             <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="RegistrarMaterias.php">Registar Materias</a></li>
+            <li><a class="dropdown-item" href="RegistrarMaterias.php">Registrar materias</a></li>
           </ul>
         </li>
+        
       </ul>
       <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Search">
-        <button class="btn btn-outline-secondary" type="submit">Buscar</button>
+        <input class="form-control me-2" type="Buscar" placeholder="Buscar" aria-label="Buscar">
+        <button class="btn btn-outline-success" type="submit">Buscar</button>
       </form>
     </div>
   </div>
